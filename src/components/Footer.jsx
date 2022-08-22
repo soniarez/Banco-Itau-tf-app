798d0c09f61d5f5878b33024e9252c7d5f9d01e9
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2022-08-22 17:53:26 -0400

    pruebita mala

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,28 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-indigo-200 text-gray-800 fixed inset-x-0 bottom-0 p-2 flex justify-between h-[60px]'>
-      <p>Logo</p>
-      <p>Normativa</p>
-      <p>Redes Sociales</p>
+      <p> Logo
+        {/* <img src="logo.png" alt="" className='logo' /> REVISAR TAMAÑO MAS TARDE*/}
+        </p>
+      <p>Normativa
+      {/* ¿Cómo podemos ayudarte?
+
+Si eres cliente del banco no dudes en comunicarte con nosotros:
+
++562 2686 0888
+tel:6006860888 
+PROBANDO*/}
+      </p>
+      <p>Redes Sociales
+{/*   ICONOS DE PRUEBA, NO SE VEN :c      
+      <li><a href="https://www.facebook.com/itauchile"> <FontAwesomeIcon icon="fa-brands fa-facebook" />Facebook</a></li>
+      <li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/itauchile/" aria-label="Instagram"><i class="icon_itau_instagram_icon h5"></i>Instagram</a></li>
+      <li><a target="_blank" rel="noreferrer" href="https://twitter.com/itauchile" aria-label="Twitter"><i class="icon_itau_twitter_icon h5"></i>Twitter</a></li>
+      <li><a target="_blank" rel="noreferrer" href="https://cl.linkedin.com/company/itauchile" aria-label="Linkedin"><i class="icon_itau_linkedin_icon h5"></i>LinkedIn</a></li>
+      <li><a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@itauchile?lang=es" aria-label="TikTok"><i class="fa-brands fa-tiktok-square"></i>TikTok</a></li>
+    */}
+       </p>
+     
     </div>
   )
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div className='bg-red-500 w-[200px] h-screen'>
+    <div className=' bg-gray-200 rounded-xl shadow border p-8 m-10'> 
+    {/* Probando el costado y sus colores y tamaños */}
       <h1>Autorizar Transacciones</h1>
       <h1>Ultimos Movimientos</h1>
     </div>
@@ -10,3 +11,35 @@ const SideBar = () => {
 }
 
 export default SideBar
+
+
+// import React from 'react'
+
+
+
+// const SideBar = () => {
+
+//   const [Toggle, setToggle] = useState(false)
+
+//     return (
+//         <>
+//           <header className='bg-blue-500 sm:flex sm:justify-center'>
+//                 <div className='flex items-center px-4 py-3 justify-between sm:pt-2'>
+//                     <div className=''>
+//                         <img className='h-6' src={LOGO} alt='LOGO'/>
+//                     </div>
+//                     <div className=' text-green-400 sm:hidden cursor-pointer' onClick={()=> setToggle(!Toggle)}>
+//                         <GoThreeBars size='2rem'/>
+//                     </div>
+//                 </div>
+//                 {Toggle ? <div className='px-2 pt-2 font-bold  sm:flex'>
+//                     <a href='https://www.google.com' className=' flex justify-center px-3 py-1 text-white hover:text-green-500 '>SkillSet</a>
+//                     <a href='https://www.google.com' className=' flex justify-center px-3 py-1 text-white hover:text-green-500 '>Curriculum</a>
+//                     <a href='https://www.google.com' className=' flex justify-center px-3 py-1 text-white hover:text-green-500 '>Contact Me</a>
+//                 </div> : '' }
+//             </header>
+//         </>
+
+//     )
+// }
+// export default SideBar
