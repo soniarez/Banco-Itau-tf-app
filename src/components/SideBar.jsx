504d8490442cfb0dfb861cc714b07e3707ce7b5c
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2022-08-24 12:29:24 -0400

    Link bonito

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,21 +7,21 @@ const SideBar = () => {
   
     <div className=' bg-gray-200 text-gray-700 w-[200px] h-screen shadow border '> 
 <div className="flex flex-col items-center w-45 h-full overflow-hidden text-gray-700 bg-gray-100 rounded">
-		<Link className="flex items-center w-full px-3 mt-3">
+		<div className="flex items-center w-full px-3 mt-3">
 			<svg className="w-8 h-8 fill-current"  viewBox="0 0 20 20" fill="currentColor">
 				</svg>
 			<div className="ml-2 text-sm font-bold">Dashboard Multiempresa</div>
-		</Link>
+		</div>
 		
 		<div className="w-full px-2">
 			<div className="flex flex-col items-center w-full mt-3 border-t border-gray-300">
 			
-				<Link className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" Link href="#">
+				<div className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" >
 					<svg className="w-6 h-6 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 					</svg>
 					<div className="ml-2 text-sm font-medium">Buscar</div>
-				</Link>
+				</div>
 
 				<Link to="/movements" className="flex items-center w-full h-12 px-3 mt-2 hover:bg-gray-300 rounded">
 					<svg className="w-6 h-6 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -40,19 +40,19 @@ const SideBar = () => {
 			</div>
 		
 			<div className="flex flex-col items-center w-full mt-2 border-t border-gray-300">
-				<Link to='https://ww2.itau.cl/inversiones' className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
+				<div className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300">
 					<svg className="w-6 h-6 stroke-current"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
 					</svg> 
-					<div className="ml-2 text-sm font-medium">Productos</div></Link>
+					<div className="ml-2 text-sm font-medium">Productos</div></div>
 				
 				
-				<Link to='https://thumbs.dreamstime.com/b/dise%C3%B1o-del-confeti-de-la-celebraci%C3%B3n-41244387.jpg' className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
+				<div className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" >
 					<svg className="w-6 h-6 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
 					</svg>
 					<div className="ml-2 text-sm font-medium">Notificaciones</div>
-					<div className="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></div></Link>
+					<div className="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></div></div>
 
 			</div>
 		</div>
